f8b89c5943378c40d6251d41ca89fd8c4d3449f5
Shivam Kalra <56111502+shivamkalra111@users.noreply.github.com>
2021-04-23 22:51:44 +0530

    Update Labels.py

--- Labels/Labels.py
+++ Labels/Labels.py
@@ -3,10 +3,14 @@ from kivymd.uix.label import MDLabel, MDIcon
 
 class TestApp(MDApp):
     def build(self):
-        label = MDLabel(text = 'Hello World', halign = 'center', theme_text_color = 'Custom',
+        
+        label1 = MDLabel(text = 'Hello World', halign = 'center', theme_text_color = 'Error',
+                        font_style = 'Caption')
+        
+        label2 = MDLabel(text = 'Hello World', halign = 'center', theme_text_color = 'Custom',
                         text_color = (1/255.0, 94/255.0, 44/255.0, 1),
                         font_style = 'Caption')
     
-        return label
+        return label1
     
 TestApp().run()
